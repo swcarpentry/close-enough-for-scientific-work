5fcb9aaa5d92d4c77c03060fbbb423897d129384
Wolfgang Bangerth <bangerth@math.tamu.edu>
2014-11-26 11:18:07 -0600

    Update graph generation and figures. A bit more text.

--- turcksin-spring-mechanics/figures/graph.sh
+++ turcksin-spring-mechanics/figures/graph.sh
@@ -7,9 +7,11 @@
    echo "node [style=filled,color=blue,label=\"\"] `echo $i | sed s/[-_]//g`;" ; 
  done ; 
  for i in *prm ; do
+   cat $i | perl -pi -e 's/^[ \t]*\n//g; s/^\#.*\n//g;' > 1
    for j in *prm ; do
      if test "$i" != "$j" ; then 
-       echo "`echo $i | sed s/[-_]//g` -- `echo $j | sed s/[-_]//g` [style=invis,len=`(echo scale=8 ; echo \`diff $i $j | wc -l\`/10) | bc`];" ; 
+       cat $j | perl -pi -e 's/^[ \t]*\n//g; s/^\#.*\n//g;' > 2
+       echo "`echo $i | sed s/[-_]//g` -- `echo $j | sed s/[-_]//g` [style=invis,len=`(echo scale=8 ; echo \`diff 1 2 | wc -l\`/10) | bc`];" ; 
      fi ; 
    done ;
  done ; 
