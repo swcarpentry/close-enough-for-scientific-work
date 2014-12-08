Make the plot 3d. Fix a sign error.

turcksin-spring-mechanics/spring.py
@@ -13,6 +13,7 @@ print settings["spring_constant"]
 from scipy.integrate import ode
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 D = 52.
 L = 2.25
@@ -23,7 +24,7 @@ g = [0.,0.,-9.81]
 
 y0 = [1.,2.,3., 4.,5.,6., 0.,0.,0., 1.,1.,1.]
 t0 = 0.
-T = 0.5
+T = 2
 
 m = [3.0, 1.0]
 
@@ -35,8 +36,8 @@ def f(t, y):
     v2 = y[9:12]
     
     dist = np.linalg.norm(p2-p1)
-    a1 = g - D*(dist-L) * (p2-p1)/dist/m[0];
-    a2 = g - D*(dist-L) * (p1-p2)/dist/m[1];
+    a1 = g - D*(dist-L) * (p1-p2)/dist/m[0];
+    a2 = g - D*(dist-L) * (p2-p1)/dist/m[1];
     return np.concatenate([v1, v2, a1, a2])             
 
 r = ode(f).set_integrator('vode',rtol=1e-6)
@@ -53,13 +54,9 @@ while r.successful() and r.t < T:
 print "time steps:",len(t)
 
 
-plt.plot(t, yarr[:,0],
-         t, yarr[:,1],
-         t, yarr[:,2],
-         t, yarr[:,3],
-         t, yarr[:,4],
-         t, yarr[:,5])
-
-plt.legend(["p1.x","p1.y", "..."])
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot(yarr[:,0], yarr[:,1], yarr[:,2], label='body 1')
+ax.plot(yarr[:,3], yarr[:,4], yarr[:,5], label='body 2')
 
 plt.show()