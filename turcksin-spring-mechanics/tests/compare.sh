396ac2459e0709013f5629312aaa60a74dbee32b
Timo Heister <timo.heister@gmail.com>
2015-01-21 15:53:46 -0500

    fix compare script

--- turcksin-spring-mechanics/tests/compare.sh
+++ turcksin-spring-mechanics/tests/compare.sh
@@ -1,15 +1,15 @@
 # loop over all .json input files
 for jsonfile in *.json ; do
   # execute our program on it and pipe the output into a file
-  python ${jsonfile} > ${jsonfile}.new
+  python ../spring.py ${jsonfile} > ${jsonfile}.new
 
   # then compare output; only ask 'diff' to report whether the
   # files are the same or not, and create output depending on this:
   # print an 'X' if there is a difference, a '.' if there isn't. this
   # ensures that test failures are easily visible
-  if (diff ${jasonfile} ${jsonfile}.new > /dev/null) ; then
-    echo " .    ${jasonfile}" ;
+  if (diff ${jsonfile}.output ${jsonfile}.new > /dev/null) ; then
+    echo " .    ${jsonfile}" ;
   else 
-    echo " X    ${jasonfile}" ;
+    echo " X    ${jsonfile}" ;
   fi
 done
